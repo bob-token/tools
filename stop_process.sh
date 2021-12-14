9714d342b2cae950eb338e7d2da0a5b96680f0f8
bob-token <xhban520@gmail.com>
2021-12-14 11:25:57 +0800

    Update stop_process.sh
    optimization:echo color

--- stop_process.sh
+++ stop_process.sh
@@ -1,54 +1,59 @@
 #!/bin/bash
-#echo pid info
 
+#color define
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+#echo pid info
 if [ -z $1 ];then
 echo please input string
 exit
 fi
 
-pid=$(ps -ef |grep $1 |grep -v grep|grep -v stop_process.sh|awk '{print $2}')
+pid=$(ps -ef |grep $1 |grep -v grep|grep -v stop_process.sh|grep -v stop_process|awk '{print $2}')
 
 total=0
 for p in ${pid}
 do
  total=$[total + 1]
 done
-
-
-
 if [ $total -eq 0 ]
 then
 echo **not found!**
 exit
 fi
 
-echo ""
-echo **CAULTION: $total PROCESSES WILL BE KILLED**
-echo ""
-
+echo -e *****************************************************************************${GREEN}
 for p in ${pid}
 do
  ps $p|grep $1 |grep -v grep
 done
+echo -e "${NC}*****************************************************************************"
+
+echo ""
+echo -e ${RED}** CAULTION: $total PROCESSES WILL BE KILLED **${NC}
+echo ""
+
 
 read  -p "kill all [yes/no]:" mainmenuinput
 
 if [[ -n $mainmenuinput && $mainmenuinput == yes ]]
 then
-
- for p in ${pid}
+for p in ${pid}
  do
  echo **kill... $p**
  ps $p |grep $1|grep -v grep
  kill $p
  tail --pid=$p -f /dev/null
  done
-
-else
+ else
 
 echo **skipped!**
 exit
 
 fi
+
 
 echo **all killed**
